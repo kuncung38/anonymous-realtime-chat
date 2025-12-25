5f0df05124df987d5edb72371a9e5d06407e2b86
Kuncung <rickowijaya@gmail.com>
2025-12-25 22:02:06 +0700

    feat: Add base URL to client treaty configuration

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,4 +2,4 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "@/app/api/[[...slug]]/route";
 import { getBaseUrl } from "./utils";
 
-export const client = treaty<App>("").api;
+export const client = treaty<App>(getBaseUrl()).api;
